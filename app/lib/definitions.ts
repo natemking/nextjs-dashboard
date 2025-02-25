0aff1290f0b53bb6e0b48a449d87a183698ae614
Nate King <nking@xtivia.com>
2025-02-25 12:10:21 -0500

    updated types to DRY code

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,6 +2,8 @@
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
+type StatusOpts = 'pending' | 'paid';
+
 export type User = {
   id: string;
   name: string;
@@ -23,7 +25,7 @@ export type Invoice = {
   date: string;
   // In TypeScript, this is called a string union type.
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
-  status: 'pending' | 'paid';
+  status: StatusOpts;
 };
 
 export type Revenue = {
@@ -52,7 +54,7 @@ export type InvoicesTable = {
   image_url: string;
   date: string;
   amount: number;
-  status: 'pending' | 'paid';
+  status: StatusOpts;
 };
 
 export type CustomersTableType = {
@@ -84,5 +86,5 @@ export type InvoiceForm = {
   id: string;
   customer_id: string;
   amount: number;
-  status: 'pending' | 'paid';
+  status: StatusOpts;
 };
