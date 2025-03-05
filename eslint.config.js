d631c7deea64932fcd900e5b6b87c45929bea1b7
Nate King <nking@xtivia.com>
2025-03-05 14:42:04 -0500

    added import eslint lib and rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import tseslint from 'typescript-eslint';
 import reactPlugin from 'eslint-plugin-react';
 import nextPlugin from '@next/eslint-plugin-next';
 import commentsPlugin from 'eslint-plugin-eslint-comments';
+import importPlugin from 'eslint-plugin-import';
 
 const compat = new FlatCompat({
 	baseDirectory: import.meta.dirname,
@@ -12,6 +13,8 @@ const compat = new FlatCompat({
 export default tseslint.config(
 	eslint.configs.recommended,
 	...compat.config(commentsPlugin.configs.recommended),
+	importPlugin.flatConfigs.recommended,
+	// importPlugin.flatConfigs.recommendedTypeScript,
 	reactPlugin.configs.flat.recommended,
 	reactPlugin.configs.flat['jsx-runtime'],
 	tseslint.configs.strictTypeChecked,
@@ -30,7 +33,7 @@ export default tseslint.config(
 				},
 			},
 		},
-        // prevent ts linting on certain config files - https://typescript-eslint.io/users/configs/#disable-type-checked
+		// prevent ts linting on certain config files - https://typescript-eslint.io/users/configs/#disable-type-checked
 		{
 			files: ['**/*.{js,mjs,cjs}'],
 			extends: [tseslint.configs.disableTypeChecked],
@@ -45,6 +48,11 @@ export default tseslint.config(
 			settings: {
 				react: {
 					version: 'detect',
+				},
+				// resolver for typescript and node imports
+				'import/resolver': {
+					typescript: true,
+					node: true,
 				},
 			},
 			// plugins: {
@@ -356,6 +364,90 @@ export default tseslint.config(
 				 * 🚫 Not fixable - https://eslint.org/docs/rules/symbol-description
 				 */
 				'symbol-description': 'error',
+
+				//imports
+				/**
+				 * Disallow non-import statements appearing before import statements.
+				 *
+				 * 🚫 Not fixable - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/first.md
+				 */
+				'import/first': 'error',
+				/**
+				 * Require a newline after the last import/require.
+				 *
+				 * 🔧 Fixable - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/newline-after-import.md
+				 */
+				'import/newline-after-import': 'warn',
+				/**
+				 * Disallow import of modules using absolute paths.
+				 *
+				 * 🚫 Not fixable - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-absolute-path.md
+				 */
+				'import/no-absolute-path': 'error',
+				/**
+				 * Disallow cyclical dependencies between modules.
+				 *
+				 * 🚫 Not fixable - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-cycle.md
+				 */
+				'import/no-cycle': 'error',
+				/**
+				 * Disallow default exports.
+				 *
+				 * 🚫 Not fixable - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-default-export.md
+				 */
+				'import/no-default-export': 'error',
+				/**
+				 * Disallow the use of extraneous packages.
+				 *
+				 * 🚫 Not fixable - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-extraneous-dependencies.md
+				 */
+				'import/no-extraneous-dependencies': [
+					'error',
+					{ includeTypes: true },
+				],
+				/**
+				 * Disallow mutable exports.
+				 *
+				 * 🚫 Not fixable - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-mutable-exports.md
+				 */
+				'import/no-mutable-exports': 'error',
+				/**
+				 * Disallow importing packages through relative paths.
+				 *
+				 * 🚫 Not fixable - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-relative-packages.md
+				 */
+				'import/no-relative-packages': 'warn',
+				/**
+				 * Disallow a module from importing itself.
+				 *
+				 * 🚫 Not fixable - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-self-import.md
+				 */
+				'import/no-self-import': 'error',
+				/**
+				 * Ensures that there are no useless path segments.
+				 *
+				 * 🚫 Not fixable - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-useless-path-segments.md
+				 */
+				'import/no-useless-path-segments': ['error'],
+				/**
+				 * Enforce a module import order convention.
+				 *
+				 * 🔧 Fixable - https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/order.md
+				 */
+				'import/order': [
+					'warn',
+					{
+						groups: [
+							'builtin', // Node.js built-in modules
+							'external', // Packages
+							'internal', // Aliased modules
+							'parent', // Relative parent
+							'sibling', // Relative sibling
+							'index', // Relative index
+						],
+						'newlines-between': 'never',
+					},
+				],
 
 				'@typescript-eslint/consistent-type-definitions': [
 					'error',
