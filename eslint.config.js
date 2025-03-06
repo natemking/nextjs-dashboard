5e649a20be70cca4f3865a0869293e8405dd1df7
Nate King <nking@xtivia.com>
2025-03-06 10:08:03 -0500

    added rules for variables

--- eslint.config.js
+++ eslint.config.js
@@ -662,6 +662,34 @@ export default tseslint.config(
                  */
                 'react/self-closing-comp': 'warn',
 
+                // variables
+                /**
+                 * Disallow labels that share a name with a variable.
+                 *
+                 * 🚫 Not fixable - https://eslint.org/docs/rules/no-label-var
+                 */
+                'no-label-var': 'error',
+                /**
+                 * Disallow initializing variables to `undefined`.
+                 *
+                 * 🔧 Fixable - https://eslint.org/docs/rules/no-undef-init
+                 */
+                'no-undef-init': 'warn',
+                /**
+                 * Disallow unused variables.
+                 *
+                 * 🚫 Not fixable - https://eslint.org/docs/rules/no-unused-vars
+                 */ 'no-unused-vars': [
+                    'error',
+                    {
+                        args: 'after-used',
+                        argsIgnorePattern: '^_',
+                        ignoreRestSiblings: false,
+                        vars: 'all',
+                        varsIgnorePattern: '^_',
+                    },
+                ],
+
                 '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
             },
         },
